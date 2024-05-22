b490c8522946112c252cf3da1382a509e0bc0704
priyanshgoyalofficial <priyanshgoyalofficial@gmail.com>
2024-05-22 12:55:51 +0530

    added media queries

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
-import cominglogo from '../assets/Images/logo.png'
-import nnbackground from '../assets/Images/vid.mp4'
+import cominglogo from '../assets/Images/new-in.svg'
+import videoBg from '../assets/Images/bg-video.mp4'
+import email from '../assets/Images/whatsapp-white.png'
+import whatsapp from '../assets/Images/email-white.png'
 
 
 const ComingSoon = () => {
   return (
-    <section>
-        <div className='container'> 
-          <div className="coming-soon">
-            <video autoPlay={true} loop={true} controls={true} src={nnbackground}></video>
+    <>
+      <div className="main">
+        <div className="overlay"></div>
+        <video src={videoBg} autoPlay loop muted />
+        <div className="content">
+          <div className="coming-logo">
+            <img src={cominglogo} alt="" />
           </div>
-          <div className="ok">
-            <div className="coming-logo">
-              <img src={cominglogo} alt="" />
-            </div>
-            <div className="coming-text">
-              <h1 className='neon-text'>coming soon</h1>
-            </div>
-            <div className="coming-sub">
-              <p>innovating with creativity</p>
-            </div>
+          <div className="coming-text">
+            <h1 className='neon-text'>coming soon</h1>
           </div>
+          <div className="coming-sub">
+            <p>innovating with creativity</p>
+          </div>
+          <div className="contact-now">
+              <a href="mailto:cmpundhir@ideaneuron.com">
+                <div className="contact-whatsapp">
+                  <img className='contact-img' src={email} alt="" />
+                  <p>cmpundhir@ideaneuron.com</p>
+                </div>
+              </a>
+              <a href="tel:859-507-3725">
+                <div className="contact-email">
+                  <img className='contact-img' src={whatsapp} alt="" />
+                  <p>+91 8595073725</p>
+                </div>
+              </a>
+            </div>
         </div>
-      </section>
+      </div>
+    </>
   )
 }
 
